a22db6c6fe123c075026fae7f47e381e2cf56aee
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-11-10 18:04:33 -0600

    render obj to handlebars engine

--- controller/baseball_controller.js
+++ controller/baseball_controller.js
@@ -1,6 +1,16 @@
-const express = require("express");
-baseball = require("../models/app");
+const express = require("express"),
+  baseball = require("../models/app");
 
 const router = express.Router();
 
-router.route("/").get(function(res, req){});
+router.route("/").get(function(req, res) {
+  baseball.allTeams(function(data) {
+    let teamsObj = {
+      teams: data
+    };
+    console.log(teamsObj);
+    res.render("index", teamsObj);
+  });
+});
+
+module.exports = router;
