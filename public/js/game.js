263d8203a1e76fab0466ab5299c80c11ae106c78
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-11-14 14:21:33 -0600

    declared scoring intervals for track score for mactch

--- public/js/game.js
+++ public/js/game.js
@@ -1,8 +1,11 @@
 
 function match(homeTeam, awayTeam, location, startMatch) {
   /*Post to matches in baseballdb  */
-  let home_team_id = 1
-  let away_team_id = 2
+  let 
+    home_team_id = 1,
+    away_team_id = 2,
+    visScoreInning = 0,
+    homeScoreInning = 0;
   startInning(home_team_id, away_team_id);
 };
 
@@ -12,25 +15,52 @@ function startInning(){
         inning = 1,
         visScoreInningText = $(`#visScore${inning}`),
         homeScoreInningText = $(`#homeScore${inning}`),
-        batterbox = $('#batter-card')
-        visScoreInning = 0,
-        homeScoreInning = 0;
+        batterbox = $('#batter-card');
+
+    visScoreInning = 0;
+    homeScoreInning = 0;
 
     batterbox.hide();
 
 
-        $('#hit-btn').on("click", function(){
-            console.log("hello")
-        })
-    Top()
-    Bottom()
-        
-        
+    atBatsTop();
 
-
+    
     visScoreInningText.text(visScoreInningText);
     homeScoreInningText.text(homeScoreInningText);
 
     inning ++;
 }
 
+function atBatsTop(){ //promises?
+    let 
+        outs = 0,
+        hits = 0;
+
+     do{
+        $('#out-btn').on('click', function() {
+         outs ++;
+            console.log(outs)   
+        });
+
+        $('#hit-btn').on('click', function() {
+            hits ++;
+            console.log(hits)
+            if(hits >= 4) {
+                visScoreInning ++
+             }
+        });
+
+
+
+     } while (outs < 3)
+    
+        atBatsBottom();
+       
+    
+
+
+    
+}
+
+atBatsBotton
