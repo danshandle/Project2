bf1a88c73f1293c9ceaf0ce57da57244c0f4d628
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2019-11-19 20:28:51 -0600

    heroku deployement

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
 const mysql = require("mysql");
 require('dotenv').config();
+let connection;
 
-if(process.env.JAWSDB_URL) {
-  const connection =mysql.createConnection(process.env.JAWSDB_URL):
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  const connection = mysql.createConnection({
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "password",
   database: "baseballdb"
-})
+});
 
-}
-
-;
+};
 
 connection.connect(function (err) {
   if (err) {
